086d71a609959dfe66837164aa4bce3fb934b8c2
NagiFish <94735845+nezikog@users.noreply.github.com>
2024-10-29 14:34:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,4 +29,31 @@ function initials(){
     surnameTxt.textContent = SurName
 }
 
+function Lessons(){
+    let lesson0 = "C#"
+    let lesson1 = "Html, Css, Js"
+
+    let Less0 = document.getElementById("en0")
+    let Less1 = document.getElementById("en1")
+    
+    Less0.textContent = lesson0
+    Less1.textContent = lesson1
+}
+
+function Time(){
+    let lesson0 = "C#"
+    let lesson1 = "Html, Css, Js"
+
+    let time0 = "10.00 до 11.30"
+    let time1 = "11.45 до 12.00"
+
+    let Less0 = document.getElementById("enn0")
+    let Less1 = document.getElementById("enn1")
+    
+    Less0.textContent = lesson0 + " " +time0
+    Less1.textContent = lesson1 + " " +time1
+}
+
+Time();
+Lessons();
 initials();
